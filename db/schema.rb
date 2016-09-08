03cbf90611761b9e949e857998a7a24abf324dbb
Felipe Soares Gonçalves Rosa <felipe.sgrosa@gmail.com>
2016-09-08 14:54:40 -0300

    added sprints table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908151029) do
+ActiveRecord::Schema.define(version: 20160908175346) do
+
+  create_table "sprints", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "due_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "squads", force: :cascade do |t|
     t.string   "name"
