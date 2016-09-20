35a7cc2852f9dabf96fff85c34ee129dd18a59ff
Felipe Soares Gonçalves Rosa <felipe.sgrosa@gmail.com>
2016-09-20 18:01:15 -0300

    fix start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,18 @@
 #! /bin/sh
 
+if [ $RAILS_ENV == "development" ]; then
+  # Espera o servidor do MySQL responder
+  until $(curl --output /dev/null --silent --head --fail http://mysql:3306); do
+      sleep 1
+  done
+fi
+
 # Configura o banco de dados
 bundle exec rails db:create
 bundle exec rails db:migrate
 # Precompile
-rails assets:precompile
+if [ $RAILS_ENV == "production" ]; then
+  rails assets:precompile
+fi
 # Liga o server
 bundle exec rails server -b 0.0.0.0 -p 80
