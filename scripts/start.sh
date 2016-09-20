048000ecc79ab3d5e926b835c57500e50bb5aafb
Diley <rodrigodiley@gmail.com>
2016-09-20 17:47:06 -0300

    add kubernetes

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,9 @@
 #! /bin/sh
 
-# Espera o servidor do MySQL responder
-until $(curl --output /dev/null --silent --head --fail http://mysql:3306); do
-    sleep 1
-done
 # Configura o banco de dados
 bundle exec rails db:create
 bundle exec rails db:migrate
+# Precompile
+rails assets:precompile
 # Liga o server
 bundle exec rails server -b 0.0.0.0 -p 80
