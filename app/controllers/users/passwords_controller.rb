620373854b8f7ee96451344ae7d97486ba760fbc
Felipe Soares Gonçalves Rosa <felipe.sgrosa@gmail.com>
2016-11-08 13:22:30 -0200

    added forgot password feature

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,42 @@
+class Users::PasswordsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    puts params
+    @user = User.find_by_email(user_params[:email])
+    @user.send_reset_password_instructions
+
+    flash[:notice] = 'E-mail de instruções enviado'
+    redirect_to :root
+  end
+
+  def edit
+    @user = User.new
+    @reset_password_token = params[:reset_password_token]
+  end
+
+  def update
+    @user = User.reset_password_by_token(user_params)
+
+    if @user.errors.empty?
+      flash[:notice] = 'Senha alterada!'
+      redirect_to :root
+    else
+      if @user.errors.details[:password_confirmation]
+        flash[:notice] = 'As senhas digitadas devem ser iguais'
+        redirect_to edit_user_password_path(reset_password_token: user_params[:reset_password_token])
+      else
+        flash[:error] = 'Não foi possível alterar a senha'
+        redirect_to :root
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :reset_password_token)
+  end
+end
