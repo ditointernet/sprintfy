3d8f63eff7736efa373331ba23e797e16b9bc214
Eduardo Gonçalves <eduardo.goncalves@dito.com.br>
2017-09-06 10:40:21 -0300

    Modificada captaçào de dados dos Reports

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,14 +4,22 @@ has_and_belongs_to_many :sprints
     def chart_data_sprint
       sprints_test = {}
       Sprint.all.each_with_object({}) do |sprint|
-        sprints_test[sprint.squad.id] = sprint.squad.name
+        sprints_test[sprint.id] = [
+          Squad.where(id: sprint.squad_id).first.name,
+          sprint.squad_id,
+          sprint.start_date.strftime("%d-%m-%y"),
+          sprint.due_date.strftime("%d-%m-%y"),
+          User.all.each_with_object({}) do |user|
+            user.id
+          end
+        ]
       end
       sprints_test
     end
 
     def admin_chart_data
       [
-        name: 'Média de SPs por dia por sprint',
+        name: 'Test data',
         data: chart_data_sprint
       ]
     end
