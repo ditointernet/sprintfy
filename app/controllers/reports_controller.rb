b4b1458caf0ab9fe77c44bba6bbc87c117062f83
Eduardo Gonçalves <eduardo.goncalves@dito.com.br>
2017-09-08 15:50:15 -0300

    Retirado o gráfico de squads da página de relatórios

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,6 +4,5 @@ class ReportsController < ApplicationController
   def index
     @report = Report.new
     @month_all_data = @report.chart_data_month_all
-    @sprint_squad_data = @report.chart_data_sprint_squad(1)
   end
 end
